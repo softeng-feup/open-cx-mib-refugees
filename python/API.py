2e0150baa4485dec0675a628dc7a80be2cfc5123
Patricia Ferreira Rocha <up201504616@fe.up.pt>
2019-10-25 14:25:07 +0100

    py update

--- python/API.py
+++ python/API.py
@@ -1,17 +1,10 @@
 import requests
-import json
+#import json
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://esof-20d6f.firebaseio.com', None)
 
 firebase.delete('/Program', None)
-'''
-Sempre que o códiogo corre são acrescentadas as mesmas informaçoes a base de dados.
-Pelo menos para já estou a fazer assim, eliminar sempre tudo antes de carregar. 
-Eventualmente vamos ter de fazer ficheros de codigo separados para carregar a
-a base de dados uma vez só e o resto do codigo noutro ficheiro
-'''
-
 
 response = requests.get("https://2019.programming-conference.org/dataexport/810b23a0-737b-4f74-9170-75d515274859/confero.json?fbclid=IwAR0jR7psUtcCvVg3zL7KnseXUVqNY5DTeLzgM9xeL3E4h74RvWMkuBz94VY")
 if response.status_code == 200:
@@ -20,20 +13,18 @@ elif response.status_code == 404:
     print('API not found.')
 
 # Convert into JSON string
-def jprint(obj):
-    text = json.dumps(obj, sort_keys=True, indent=4)
+#def jprint(obj):
+    #text = json.dumps(obj, sort_keys=True, indent=4)
     #print(text)
 
-jprint(response.json())
+#jprint(response.json())
 
 Items=response.json()['Items']
 
-Titles = []
+Titles=[]
 Type=[]
 Person=[]
-Affiliation=[]
-Abstract=[]
-number = 0 #para recolher apenas as primeiras 20
+number = 0
 
 for d in Items:
     person=d['PersonsString']
@@ -44,48 +35,42 @@ for d in Items:
               title=d['Title']
               type=d['Type']
               person=d['PersonsString']
-              affiliation=d['AffiliationsString']
-              abstract=d['Abstract']
+              
               Titles.append(title)
               Type.append(type)
               Person.append(person)
-              Affiliation.append(affiliation)
-              Abstract.append(abstract)
          else:
                break
                   
 '''
-Categorias e respetivas lectures atribuidas
-1.Coding dojo-14,16
-2.Web-7,11,13,
-3.Cybersecurity-5
-4.Artificial Intelligence-8,9
-5.Internet of Things-6,20
-6.UI/UX
-7.Criptocurrency
-8.Virtual Machines- 15
-9.Computational optimization-10,18,
-10.Object-oriented programming-1,17
-11.Other-2,3,4,12,19
+Categories // lectures 
 
-*rever as outras, porque nao sei onde as por
+1. Coding dojo // 14,16
+2. Web // 7,11,13
+3. Cybersecurity // 5
+4. Artificial Intelligence // 8,9
+5. Internet of Things // 6,20
+6. UI/UX
+7. Cryptocurrency
+8. Virtual Machines // 15
+9. Other // 1,2,3,4,10,12,17,18,19
+
 ''' 
   
-Cat = [10, 11, 11, 11, 3, 5, 2, 4, 4, 9, 2, 11, 2, 1, 8, 1, 10, 9, 11, 5]
+categories = [9, 9, 9, 9, 3, 5, 2, 4, 4, 9, 2, 9, 2, 1, 8, 1, 9, 9, 9, 5]
 
 n=0
-while n < len(Cat):
+while n < len(categories):
     person=Person[n]
+    category=categories[n]
     title=Titles[n]
     type=Type[n]
-    cat=Cat[n]
     data = {'Author': person,
-                 'Title': title,
-                 'Type': type,
-                 'Category': cat
-                 }
+            'Category': category,
+            'Title': title,
+            'Type': type
+            }
     firebase.post('/Program', data)
     n += 1
 
-#verificar que a base de dados tem a informação desejada
 db=firebase.get('/Program', None)
